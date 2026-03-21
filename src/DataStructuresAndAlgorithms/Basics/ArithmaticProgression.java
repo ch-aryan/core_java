df2db54ac74f5605dfc2b65c65e71fb746c9dbc0
Aryan <aryanprabha650@gmail.com>
2026-03-21 23:18:20 +0530

    Add program to find sum of Arithmetic Progression (AP)

---
+++ src/DataStructuresAndAlgorithms/Basics/ArithmaticProgression.java
@@ -0,0 +1,22 @@
+package DataStructuresAndAlgorithms.Basics;
+
+public class ArithmaticProgression {
+
+        public static int sumAP(int a, int d, int n) {
+
+            int sum = n * (2 * a + (n - 1) * d) / 2;
+
+            return sum;
+        }
+
+        public static void main(String[] args) {
+
+            int a = 3;   // first term
+            int d = 3;   // common difference
+            int n = 10;  // number of terms
+
+            int result = sumAP(a, d, n);
+
+            System.out.println("Sum of AP = " + result);
+        }
+    }
