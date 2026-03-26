f528b83bbe16dc43b28a4b07f78e339bd8fccf64
Aryan <aryanprabha650@gmail.com>
2026-03-27 01:26:12 +0530

    dsa consistency beats talent

---
+++ src/DataStructuresAndAlgorithms/Basics/NaveenReddyTelusko/Arrays/RemoveDuplicatesSortedArr.java
@@ -0,0 +1,28 @@
+package DataStructuresAndAlgorithms.Basics.NaveenReddyTelusko.Arrays;
+
+public class RemoveDuplicatesSortedArr {
+
+        public int removeDuplicates(int[] nums) {
+
+            int count = 0;
+
+            for(int i =1;i<nums.length;i++){
+                if(nums[count] != nums[i]){
+                    count += 1;
+                    nums[count] = nums[i];
+                }
+            }
+
+            return count+1; // leetcode problem 26 question.
+        }
+
+    public static void main(String[] args) {
+
+
+    }
+}
+/* 👉 Correct approach:
+
+✔ O(n) time
+✔ O(1) space
+✔ Interview ready*/
