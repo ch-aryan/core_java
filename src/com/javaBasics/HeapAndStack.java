d3f65050126cfa76b33cbf48b15c88523240d9b6
Aryan <aryanprabha650@gmail.com>
2026-06-21 23:24:27 +0530

    keep going champ we can do it

---
+++ src/com/javaBasics/HeapAndStack.java
@@ -0,0 +1,30 @@
+package com.javabasics;
+class ClassAb{
+
+}
+class ClassBa{
+
+}
+public class HeapAndStack {
+    public static void main(String[] args) {
+        String a = "a";
+        System.out.println("in java stirngs are intilazied with double quotes.");
+
+        int ABC = 123;
+        int abc = 321;
+        System.out.println(abc);//here both are diff.
+        System.out.println(ABC);
+        System.out.println("Haan bhai, Java aur JavaScript dono case-sensitive languages hain.");
+
+        if(true){
+            System.out.println("hello true");
+        }else{
+            System.out.println("hello false");
+        }
+        boolean i = false;
+        while(i==false){
+            System.out.println("hello true while looop");
+
+        }
+    }
+}
