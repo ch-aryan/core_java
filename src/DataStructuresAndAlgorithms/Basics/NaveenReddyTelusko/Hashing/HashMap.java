48f1a25b013f73bcbe8d5576a8b25185d45688f5
Aryan <aryanprabha650@gmail.com>
2026-06-30 14:41:38 +0530

    keep going champ we can do it

---
+++ src/DataStructuresAndAlgorithms/Basics/NaveenReddyTelusko/Hashing/HashMap.java
@@ -0,0 +1,13 @@
+package DataStructuresAndAlgorithms.Basics.NaveenReddyTelusko.Hashing;
+
+public class HashMap {
+    /*
+    HashMap ke methods hote hain:
+
+map.containsKey(key)
+map.containsValue(value)
+map.get(key)
+
+Tumhe frequency check karni hai, isliye get() use karna padega.
+     */
+}
