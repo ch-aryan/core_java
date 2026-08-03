36f5d2aa007c8317667f9297612e435628edc426
Aryan <aryanprabha650@gmail.com>
2026-08-03 23:10:34 +0530

    started dsa stacks and queues

---
+++ src/com/BankingProject/oops/Strings.java
@@ -0,0 +1,10 @@
+package com.BankingProject.oops;
+
+public class Strings {
+    public static void main(String[] args) {
+        separator('=');
+    }
+    public static void separator(char symbol) {
+        System.out.println(String.valueOf(symbol).repeat(50));
+    }
+}
