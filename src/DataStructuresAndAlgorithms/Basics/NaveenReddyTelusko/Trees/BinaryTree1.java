b7b72e64ea797bd188da0bba2acf143440146974
Aryan <aryanprabha650@gmail.com>
2026-08-19 23:45:28 +0530

    lets start

---
+++ src/DataStructuresAndAlgorithms/Basics/NaveenReddyTelusko/Trees/BinaryTree1.java
@@ -0,0 +1,14 @@
+package DataStructuresAndAlgorithms.Basics.NaveenReddyTelusko.Trees;
+class Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(int data){
+        this.data = data;
+    }
+}
+public class BinaryTree1 {
+    public static void main(String[] args) {
+
+    }
+}
