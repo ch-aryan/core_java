1eacda739a7e431dbc69870ee26b93ac672d27bb
Aryan <aryanprabha650@gmail.com>
2026-08-21 23:42:04 +0530

    tree day2

---
+++ src/DataStructuresAndAlgorithms/Basics/NaveenReddyTelusko/Trees/BinaryTreeIterator1.java
@@ -0,0 +1,29 @@
+package DataStructuresAndAlgorithms.Basics.NaveenReddyTelusko.Trees;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+class Solution1{
+    public ArrayList<Integer> iteratorTree(Node root){
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(root);
+        ArrayList<Integer> arr = new ArrayList<>();
+        while(!stack.isEmpty()){
+            Node curr = stack.pop();
+            arr.add(curr.data);
+            if(curr.right != null){
+                stack.add(curr.right);
+            }
+            if(curr.left != null){
+                stack.add(curr.left);
+            }
+        }
+        return arr;
+    }
+}
+public class BinaryTreeIterator1 {
+    public static void main(String[] args) {
+
+    }
+}
